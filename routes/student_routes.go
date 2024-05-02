e3fe5133d12eef475706104758ab2ff45c9c3a7d
Harshith reddy <harshith@skiff.com>
2024-05-02 09:18:40 +0530

    fix(api): monitor

--- routes/student_routes.go
+++ routes/student_routes.go
@@ -2,9 +2,11 @@ package routes
 
 import (
 	"github.com/U-to-E/dashboard/controller"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"github.com/gofiber/fiber/v3"
 )
 
 func SetupStudentRoutes(app *fiber.App) {
 	app.Get("/", controller.RenderLogin)
+	app.Get("/dashboard", monitor.New())
 }
